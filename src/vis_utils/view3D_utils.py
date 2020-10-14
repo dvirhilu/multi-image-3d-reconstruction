78bb45515c766d297988e0d9d0ab1974c016d1a3
dvirhilu <dvirhilu16@gmail.com>
2020-10-14 02:56:17 -0700

    started exploring 3D viewer

---
+++ src/vis_utils/view3D_utils.py
@@ -0,0 +1,11 @@
+'''
+Multi-Image 3D Reconstruction
+3D Viewer Utils
+
+This script contains a set of useful helper functions to 
+allow the reconstructed object to be viewed in 3D rotated
+view.
+
+Author:   Dvir Hilu
+Date:     14/10/2020
+'''
